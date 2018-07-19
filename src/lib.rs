2a66dce153cb9a41c7a8afd93e266e0add1c4750
Martin Billinger <flkazemakase@gmail.com>
2018-07-19 21:48:47 +0200

    feat: first struct skeletons

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,104 @@
+extern crate cpal;
+pub extern crate rodio;
+
+use std::time::Duration;
+
+use cpal::{Sample as CpalSample, SampleFormat};
+use rodio::{Sample, Source};
+
+#[derive(Copy, Clone)]
+struct Bformat {
+    w: f32,
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Sample for Bformat {
+    fn lerp(first: Self, second: Self, numerator: u32, denominator: u32) -> Self {
+        unimplemented!()
+    }
+
+    fn amplify(self, value: f32) -> Self {
+        unimplemented!()
+    }
+
+    fn saturating_add(self, other: Self) -> Self {
+        unimplemented!()
+    }
+
+    fn zero_value() -> Self {
+        unimplemented!()
+    }
+}
+
+// Why? Oh, why!?
+unsafe impl CpalSample for Bformat {
+    fn get_format() -> SampleFormat {
+        panic!("The B-format is not intended to be used as a CPAL sample directly. Use a renderer instead.")
+    }
+
+    fn to_f32(&self) -> f32 {
+        panic!("The B-format is not intended to be used as a CPAL sample directly. Use a renderer instead.")
+    }
+
+    fn to_i16(&self) -> i16 {
+        panic!("The B-format is not intended to be used as a CPAL sample directly. Use a renderer instead.")
+    }
+
+    fn to_u16(&self) -> u16 {
+        panic!("The B-format is not intended to be used as a CPAL sample directly. Use a renderer instead.")
+    }
+
+    fn from<S>(_s: &S) -> Self
+    where
+        S: CpalSample,
+    {
+        panic!("The B-format is not intended to be used as a CPAL sample directly. Use a renderer instead.")
+    }
+}
+
+struct BstreamStereoRenderer {
+    buffered_sample: Option<f32>,
+    bstream: Source<Item = Bformat>,
+}
+
+impl Source for BstreamStereoRenderer {
+    #[inline(always)]
+    fn current_frame_len(&self) -> Option<usize> {
+        self.bstream.current_frame_len()
+    }
+
+    #[inline(always)]
+    fn channels(&self) -> u16 {
+        2 // well, it's stereo...
+    }
+
+    #[inline(always)]
+    fn sample_rate(&self) -> u32 {
+        self.bstream.sample_rate()
+    }
+
+    #[inline(always)]
+    fn total_duration(&self) -> Option<Duration> {
+        self.bstream.total_duration()
+    }
+}
+
+impl Iterator for BstreamStereoRenderer {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.buffered_sample.take() {
+            Some(s) => Some(s),
+            None => {
+                let sample = self.bstream.next()?;
+                unimplemented!()
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
